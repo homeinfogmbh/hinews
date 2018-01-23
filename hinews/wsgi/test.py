1dbaaa192a5b349a41535c0cf4cd10f1320536dc
Richard Neumann <r.neumann@homeinfo.de>
2018-01-23 11:31:42 +0100

    Added further tests.

--- hinews/wsgi/test.py
+++ hinews/wsgi/test.py
@@ -1,9 +1,11 @@
 """Testing stuff."""
 
-from his import ACCOUNT
-from his.orm import Account
+from peewee import DoesNotExist
 
 from wsgilib import JSON
+
+from his import ACCOUNT
+from his.orm import Account
 
 __all__ = ['ROUTES']
 
@@ -12,6 +14,16 @@ def get_account():
     """Returns the actual account ORM model's instance."""
 
     return Account.get(Account.id == ACCOUNT.id)
+
+
+def get_test_left():
+
+    return Account.get(Account.id == ACCOUNT)
+
+
+def get_test_right():
+
+    return Account.get(ACCOUNT == Account.id)
 
 
 def test(arg):
@@ -26,6 +38,22 @@ def test(arg):
             'ACCOUNT == account': ACCOUNT == account,
             'ACCOUNT.id == account.id': ACCOUNT.id == account.id,
             'account.id == ACCOUNT.id': account.id == ACCOUNT.id})
+    elif arg == 'get':
+        try:
+            account_left = get_test_left()
+        except DoesNotExist:
+            account_left = None
+        else:
+            account_left = account_left.to_dict()
+
+        try:
+            account_right = get_test_right()
+        except DoesNotExist:
+            account_right = None
+        else:
+            account_right = account_left.to_dict()
+
+        return JSON({'left': account_left, 'right': account_right})
 
     return 'Invalid argument: {}.'.format(arg)
 
