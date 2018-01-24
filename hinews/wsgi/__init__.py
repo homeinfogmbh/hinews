74879a6a74db90ea56f871f0b5b9f0783efbd925
Richard Neumann <r.neumann@homeinfo.de>
2018-01-24 11:39:07 +0100

    Removed test stuff.

--- hinews/wsgi/__init__.py
+++ hinews/wsgi/__init__.py
@@ -2,9 +2,9 @@
 
 from wsgilib import Application
 
-from hinews.wsgi import article, customer, image, public, tag, test
+from hinews.wsgi import article, customer, image, public, tag
 
 APPLICATION = Application('hinews', debug=True, cors=True)
 APPLICATION.add_routes(
     article.ROUTES + customer.ROUTES + image.ROUTES + public.ROUTES
-    + tag.ROUTES + test.ROUTES)
+    + tag.ROUTES)
