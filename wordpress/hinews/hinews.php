c216933bc7ca64febf0e3080cd645adde09ad2fc
Richard Neumann <r.neumann@homeinfo.de>
2018-09-06 16:26:37 +0200

    Fix.

--- wordpress/hinews/hinews.php
+++ wordpress/hinews/hinews.php
@@ -20,14 +20,21 @@ add_shortcode('hinews', 'hinews_shortcode');
 
 
 function hinews_shortcode(){
-	wp_enqueue_style('hinews.css', plugins_url('hinews.css', __FILE__));
+    wp_enqueue_style('hinews.css', plugins_url('hinews.css', __FILE__));
     wp_enqueue_script('hinews.js', plugins_url('hinews.js', __FILE__));
+    $options = get_option('homeinfo_news_options');
+    wp_localize_script('hinews.js', 'php_vars', $options);
+    $base_url = 'https://backend.homeinfo.de/hinews/pub/article?access_token=';
+    $request_url = $base_url . $options['token'];
+    $response = file_get_contents($request_url);
 
-	$options = get_option('homeinfo_news');
-	wp_localize_script('hinews.js', 'php_vars', $options);
+    if ($response === FALSE) {
+        $response = '';
+    } else {
+        $response = '<pre>' . $response . '</pre>';
+    }
 
-	$template = file_get_contents(plugin_dir_path( __FILE__ )."hinews.html");
-	return $template;
-
+    $template = file_get_contents(plugin_dir_path( __FILE__ )."hinews.html");
+    return $template . $response;
 }
 ?>
