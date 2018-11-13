9cb0880ef01a421dae3072a2495e6892046b872b
Richard Neumann <r.neumann@homeinfo.de>
2018-11-13 15:50:59 +0100

    Fix.

--- hinews/__init__.py
+++ hinews/__init__.py
@@ -1,6 +1,12 @@
 """News Content Management System based on HIS."""
 
+from hinews.orm import Article, Whitelist, AccessToken
 from hinews.wsgi import APPLICATION, LOCAL_APPLICATION
 
 
-__all__ = ['APPLICATION', 'LOCAL_APPLICATION']
+__all__ = [
+    'APPLICATION',
+    'LOCAL_APPLICATION',
+    'Article',
+    'Whitelist',
+    'AccessToken']
