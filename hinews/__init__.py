98277563a9eaa5b09f8da60682d72b43d72abceb
Richard Neumann <r.neumann@homeinfo.de>
2019-02-05 10:51:26 +0100

    Refac.

--- hinews/__init__.py
+++ hinews/__init__.py
@@ -1,6 +1,11 @@
 """News Content Management System based on HIS."""
 
-from hinews.orm import article_active, Article, Whitelist, AccessToken
+from hinews.orm import article_active
+from hinews.orm import AccessToken
+from hinews.orm import Article
+from hinews.orm import Image
+from hinews.orm import Tag
+from hinews.orm import Whitelist
 from hinews.wsgi import APPLICATION, LOCAL_APPLICATION
 
 
@@ -8,6 +13,8 @@ __all__ = [
     'APPLICATION',
     'LOCAL_APPLICATION',
     'article_active',
+    'AccessToken',
     'Article',
-    'Whitelist',
-    'AccessToken']
+    'Image',
+    'Tag',
+    'Whitelist']
