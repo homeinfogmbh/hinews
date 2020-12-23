64eff0691a09eea655fc9ab46e62d0e4071b2166
Richard Neumann <mail@richard-neumann.de>
2020-12-23 19:05:56 +0100

    Added type hints.

--- hinews/wsgi/preview.py
+++ hinews/wsgi/preview.py
@@ -1,5 +1,9 @@
 """HIS-internal preview."""
 
+from typing import Iterable, Set
+
+from peewee import Expression
+
 from his import authenticated
 from wsgilib import JSON, Binary
 
@@ -14,27 +18,27 @@ __all__ = ['ROUTES']
 PREVIEW_TAGS = ('CMS',)
 
 
-def _preview_article_ids():
+def _preview_article_ids() -> Set[int]:
     """Yields allowed preview articles."""
 
     return set(atag.article_id for atag in Tag.select().where(
         Tag.tag << PREVIEW_TAGS))
 
 
-def _condition():
+def _condition() -> Expression:
     """Returns the article selection condition."""
 
     return (Article.id << _preview_article_ids()) & article_active()
 
 
-def _preview_articles():
+def _preview_articles() -> Iterable[Article]:
     """Yields allowed preview articles."""
 
     return Article.select().where(_condition()).order_by(
         Article.created).limit(4)
 
 
-def _get_article(ident):
+def _get_article(ident: int) -> Article:
     """Returns the respective preview article."""
 
     for article in _preview_articles():
@@ -44,22 +48,22 @@ def _get_article(ident):
     raise NO_SUCH_ARTICLE
 
 
-def _get_image(ident):
+def _get_image(ident: int) -> Image:
     """Returns the respective image."""
 
     try:
-        article_image = Image.get(Image.id == ident)
+        image = Image.get(Image.id == ident)
     except Image.DoesNotExist:
         raise NO_SUCH_IMAGE from None
 
-    if article_image.article in _preview_articles():
-        return article_image
+    if image.article in _preview_articles():
+        return image
 
     raise NO_SUCH_IMAGE
 
 
 @authenticated
-def list_():
+def list_() -> JSON:
     """Lists the respective news."""
 
     return JSON([
@@ -67,14 +71,14 @@ def list_():
 
 
 @authenticated
-def get_article(ident):
+def get_article(ident: int) -> JSON:
     """Returns the respective article."""
 
     return JSON(_get_article(ident).to_json(preview=True))
 
 
 @authenticated
-def get_image(ident):
+def get_image(ident: int) -> Binary:
     """Returns the respective image."""
 
     try:
